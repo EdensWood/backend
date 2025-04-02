2c943e465caed2894ee9c14eeac579517fd2ad19
EdensWood <woodsleafy@gmail.com>
2025-04-02 08:42:37 +0300

    made some changes

--- dist/config/database.js
+++ dist/config/database.js
@@ -7,9 +7,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
     logging: false,
 });
 exports.default = sequelize;
