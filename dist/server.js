1ef0f18fbdbb1818287528cb55484ae7e459bba4
EdensWood <woodsleafy@gmail.com>
2025-04-03 09:31:13 +0300

    debugging the server side

--- dist/server.js
+++ dist/server.js
@@ -83,6 +83,10 @@ app.use((0, express_session_1.default)({
 // =====================
 app.use(express_1.default.json({ limit: "10mb" }));
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 // =====================
 // 6. Apollo Server Setup
 // =====================
--- src/server.ts
+++ src/server.ts
@@ -93,6 +93,10 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 // =====================
 // 6. Apollo Server Setup
 // =====================
