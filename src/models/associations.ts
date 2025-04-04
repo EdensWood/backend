be6c9ea7ffbbf535fd85c850043bfab4d4a4a696
EdensWood <woodsleafy@gmail.com>
2025-04-04 21:42:00 +0300

    debugging the server side

--- src/models/associations.ts
+++ src/models/associations.ts
@@ -2,6 +2,6 @@ import { User } from "./user";
 import { Task } from "./task";
 
 export function setupAssociations() {
-  User.hasMany(Task, { foreignKey: "userId", as: "userTasks" }); // Changed alias to 'userTasks'
-  Task.belongsTo(User, { foreignKey: "userId", as: "taskOwner" }); // Changed alias to 'taskOwner'
+  User.hasMany(Task, { foreignKey: "userId", as: "tasks" }); // Changed alias to 'userTasks'
+  Task.belongsTo(User, { foreignKey: "userId", as: "user" }); // Changed alias to 'taskOwner'
 }
